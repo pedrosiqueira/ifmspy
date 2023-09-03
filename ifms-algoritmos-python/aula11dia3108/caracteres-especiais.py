79e3fd8988d01dd6894c1e9059a9e6eae763c2e2
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2023-09-03 18:04:34 -0400

    modified:   "01 L\303\263gica de programa\303\247\303\243o.html" 	modified:   02 Preparando o ambiente de desenvolvimento.html 	modified:   "03 Opera\303\247\303\265es aritm\303\251ticas.html" 	modified:   "04 Entrada e sa\303\255da.html" 	modified:   "05 Resto da divis\303\243o.html" 	modified:   "06 Tomando decis\303\265es.html" 	new file:   07 Caracteres.html 	new file:   08 Aleatoriedade.html 	new file:   "09 Repeti\303\247\303\243o while.html" 	new file:   ifms-algoritmos-python/aula11dia3108/c7e4.py 	new file:   ifms-algoritmos-python/aula11dia3108/caracteres-especiais.py 	new file:   ifms-algoritmos-python/aula12dia0109/formatando-numeros.py 	new file:   ifms-algoritmos-python/aula12dia0109/placeholders.py 	new file:   ifms-algoritmos-python/aula12dia0109/print-personalizado.py

---
+++ ifms-algoritmos-python/aula11dia3108/caracteres-especiais.py
@@ -0,0 +1,8 @@
+print("um texto\0outro texto")
+print("um texto\aoutro texto")
+print("um texto\b\b\b\boutro texto")
+print("um texto\toutro texto")
+print("um texto\noutro texto")
+print("um texto\voutro texto")
+print("um texto\foutro texto")
+print("um texto\routro texto")
---
+++ ifms-algoritmos-python/aula12dia0109/print-personalizado.py
@@ -0,0 +1,3 @@
+print("Uma sequência de números: ", 1, 2, 3, sep="<", end=". ")
+print("Agora, de trás para frente: ", 3, 2, 1, sep=">")
+print("Fim!")
