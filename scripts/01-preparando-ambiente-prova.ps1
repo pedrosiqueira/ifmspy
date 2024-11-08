5f8ce5267c435aeb5cb02def45ef1f0970c9f355
pedrosiqueira <pedro.siqueira@ifms.edu.br>
2024-11-08 15:44:56 -0400

    Update 01-preparando-ambiente-prova.ps1

--- scripts/01-preparando-ambiente-prova.ps1
+++ scripts/01-preparando-ambiente-prova.ps1
@@ -1,31 +1,33 @@
 # exclui o conteúdo da pasta do usuário
-Remove-Item -Path "C:\Users\aluno\*" -Recurse -Force
-
-$script1 = "https://raw.githubusercontent.com/pedrosiqueira/ifmspy/refs/heads/main/scripts/02-instalando-ambiente-python.ps1"
-$script2 = "https://raw.githubusercontent.com/pedrosiqueira/ifmspy/refs/heads/main/scripts/03-preparando-ambiente-python.ps1"
-$script3 = "https://raw.githubusercontent.com/pedrosiqueira/ifmspy/refs/heads/main/scripts/04-baixando-prova.ps1"
+Remove-Item -Path "C:\Users\aluno.PDC-ACADEMICO\*" -Recurse -Force
 
 # define a política de execução de script desta sessão atual de terminal para RemoteSigned (permissão de executar scripts) sem solicitar confirmação (-Force)
 Set-ExecutionPolicy RemoteSigned -Scope Process -Force;
 
-# executa o script online
+# define os scripts
+$script1 = "https://raw.githubusercontent.com/pedrosiqueira/ifmspy/refs/heads/main/scripts/02-instalando-ambiente-python.ps1"
+$script2 = "https://raw.githubusercontent.com/pedrosiqueira/ifmspy/refs/heads/main/scripts/03-preparando-ambiente-python.ps1"
+$script3 = "https://raw.githubusercontent.com/pedrosiqueira/ifmspy/refs/heads/main/scripts/04-baixando-prova.ps1"
+
+# executa o script1
 (New-Object System.Net.WebClient).DownloadString($script1) | Invoke-Expression
 
 # Define o código para abrir uma nova instância do PowerShell sem privilégios elevados
 $code1 = "Set-ExecutionPolicy RemoteSigned -Scope Process -Force; (New-Object System.Net.WebClient).DownloadString('$script2') | Invoke-Expression; exit"
 $code2 = "Set-ExecutionPolicy RemoteSigned -Scope Process -Force; (New-Object System.Net.WebClient).DownloadString('$script3') | Invoke-Expression; exit"
 
-# Abre o PowerShell no contexto do usuário atual, sem privilégios administrativos, e executa o script
-Start-Process -FilePath "powershell" -ArgumentList "-NoProfile", "-NoExit", "-Command", $code1 -Verb RunAsUser
-Start-Process -FilePath "powershell" -ArgumentList "-NoProfile", "-NoExit", "-Command", $code2 -Verb RunAsUser
-
-# $username = "aluno"
-# $password = ConvertTo-SecureString "alunoifms" -AsPlainText -Force
-# $credential = New-Object System.Management.Automation.PSCredential($username, $password)
+# define as credenciais
+$username = "PDC-ACADEMICO\aluno"
+$password = ConvertTo-SecureString "alunoifms" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($username, $password)
 
 # Executa o código em um novo processo com as credenciais especificadas
-# Start-Process powershell -Credential $credential -ArgumentList "-NoProfile", "-Command", $code1
-# Start-Process powershell -Credential $credential -ArgumentList "-NoProfile", "-Command", $code2
+Start-Process powershell -Credential $credential -ArgumentList "-NoProfile", "-Command", $code1
+Start-Process powershell -Credential $credential -ArgumentList "-NoProfile", "-Command", $code2
+
+# Abre o PowerShell no contexto do usuário atual, sem privilégios administrativos, e executa o script
+# Start-Process -FilePath "powershell" -ArgumentList "-NoProfile", "-NoExit", "-Command", $code1 -Verb RunAsUser
+# Start-Process -FilePath "powershell" -ArgumentList "-NoProfile", "-NoExit", "-Command", $code2 -Verb RunAsUser
 
 # exclui o conteúdo da pasta temporária
 # Remove-Item -Path "$env:TEMP\*" -Recurse -Force
