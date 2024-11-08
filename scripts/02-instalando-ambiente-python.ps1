51ca14567d2a8d4903da06296e5402da34d3dfac
pedrosiqueira <pedro.siqueira@ifms.edu.br>
2024-11-08 15:50:31 -0400

    Update 02-instalando-ambiente-python.ps1

--- scripts/02-instalando-ambiente-python.ps1
+++ scripts/02-instalando-ambiente-python.ps1
@@ -2,4 +2,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # instalação dos programas para desenvolvimento python
-choco install git python vscode
+choco install git python vscode -y
