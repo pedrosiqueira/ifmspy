2ec86854d2a16acb58f5fceab2299fa91c11acea
pedrosiqueira <pedro.siqueira@ifms.edu.br>
2024-11-08 14:45:34 -0400

    Update 01-preparando-ambiente-prova.ps1

--- scripts/01-preparando-ambiente-prova.ps1
+++ scripts/01-preparando-ambiente-prova.ps1
@@ -5,6 +5,10 @@ Remove-Item -Path "C:\Users\aluno\*" -Recurse -Force
 
 $username = "aluno"
 $password = ConvertTo-SecureString "alunoifms" -AsPlainText -Force
+$script1 = "https://raw.githubusercontent.com/pedrosiqueira/ifmspy/refs/heads/main/scripts/02-instalando-ambiente-python.ps1"
+$script2 = "https://raw.githubusercontent.com/pedrosiqueira/ifmspy/refs/heads/main/scripts/03-preparando-ambiente-python.ps1"
+$script3 = "https://raw.githubusercontent.com/pedrosiqueira/ifmspy/refs/heads/main/scripts/04-baixando-prova.ps1"
+
 
 # executa um comando com outra credencial solicitada
 # Start-Process powershell -Credential (Get-Credential) -ArgumentList "SeuComando"
@@ -20,9 +24,19 @@ $password = ConvertTo-SecureString "alunoifms" -AsPlainText -Force
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 # executa o script
-& "\\10.8.32.3\arquivos\Arquivos curso superior\tads-algoritmos\02-instalando-ambiente-python.ps1"
+# & "\\10.8.32.3\arquivos\Arquivos curso superior\tads-algoritmos\02-instalando-ambiente-python.ps1"
+(New-Object System.Net.WebClient).DownloadString('$script1') | Invoke-Expression
+
 
 # executa os scripts com a credencial configurada
-$credential = New-Object System.Management.Automation.PSCredential($username, $password)
-Start-Process -FilePath "powershell" -ArgumentList "-File '\\10.8.32.3\arquivos\Arquivos curso superior\tads-algoritmos\03-preparando-ambiente-python.ps1'" -Credential $credential -NoNewWindow
-Start-Process -FilePath "powershell" -ArgumentList "-File '\\10.8.32.3\arquivos\Arquivos curso superior\tads-algoritmos\04-baixando-prova.ps1'" -Credential $credential -NoNewWindow
+# $credential = New-Object System.Management.Automation.PSCredential($username, $password)
+# Start-Process -FilePath "powershell" -ArgumentList "-File '\\10.8.32.3\arquivos\Arquivos curso superior\tads-algoritmos\03-preparando-ambiente-python.ps1'" -Credential $credential -NoNewWindow
+# Start-Process -FilePath "powershell" -ArgumentList "-File '\\10.8.32.3\arquivos\Arquivos curso superior\tads-algoritmos\04-baixando-prova.ps1'" -Credential $credential -NoNewWindow
+
+# Define o código para abrir uma nova instância do PowerShell sem privilégios elevados
+$code1 = "(New-Object System.Net.WebClient).DownloadString('$script2') | Invoke-Expression"
+$code2 = "(New-Object System.Net.WebClient).DownloadString('$script3') | Invoke-Expression"
+
+# Abre o PowerShell no contexto do usuário atual, sem privilégios administrativos, e executa o script
+Start-Process -FilePath "powershell" -ArgumentList "-NoProfile", "-NoExit", "-Command", $code1 -Verb RunAsUser
+Start-Process -FilePath "powershell" -ArgumentList "-NoProfile", "-NoExit", "-Command", $code2 -Verb RunAsUser
