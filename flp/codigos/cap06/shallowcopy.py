b55b279e9e9877775f30bc9b8b4803ed10290c84
opedrosiqueira <opedrosiqueira@gmail.com>
2024-11-22 17:37:12 -0400

    up

--- flp/codigos/cap06/shallowcopy.py
+++ flp/codigos/cap06/shallowcopy.py
@@ -3,14 +3,18 @@ from copy import copy, deepcopy
 
 Point = make_dataclass("Point", ("x", "y"))
 
-original = [Point(0, 5), Point(-3, 6)]
+original = [Point(0, 0), Point(1, 1)]
 reference = original
 shallowcopied1 = original.copy()
 shallowcopied2 = copy(original)
 deepcopied = deepcopy(original)
 
-original.append(Point(0, 0))
-original[0].x, original[0].y = 1, 1
+original.append(Point(2, 4))
+original[0].x, original[0].y = 3, 9
+
+shallowcopied1.append(Point(4, 16))
+
+shallowcopied2[1].x, original[1].y = 5, 25
 
 print("Original Points:      ", original)
 print("Reference Points:     ", reference)
---
+++ scripts/02-instalando-ambiente-python.ps1
@@ -0,0 +1,5 @@
+# instala o chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# instalação dos programas para desenvolvimento python
+choco install git python vscode -y
